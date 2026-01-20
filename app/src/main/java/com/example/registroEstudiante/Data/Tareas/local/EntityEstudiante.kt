efc2b6b845b1c6f2f874a28ab773ca4b162ddd17
Imanol-Acosta <ilacosta05@hotmail.com>
2026-01-20 18:49:08 -0400

    Agregando EstudianteDao y EntityEstudiante

---
+++ app/src/main/java/com/example/registroEstudiante/Data/Tareas/local/EntityEstudiante.kt
@@ -0,0 +1,14 @@
+package com.example.registroEstudiante.Data.Tareas.local
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "estudiantes")
+
+data class EstudianteEntity(
+    @PrimaryKey(autoGenerate = true)
+    val estudianteId: Int = 0,
+    val nombres: String,
+    val email: String,
+    val edad: Int
+)
