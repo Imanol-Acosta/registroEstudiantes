524115a0cfe2e8326700f854a9a918f9476b074c
Imanol-Acosta <ilacosta05@hotmail.com>
2026-01-31 12:25:12 -0400

    modificando dbestudiante

--- app/src/main/java/edu/ucne/registrodeestudiantes/Data/DataBase/DBestudiante.kt
+++ app/src/main/java/edu/ucne/registrodeestudiantes/Data/DataBase/DBestudiante.kt
@@ -4,14 +4,13 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import edu.ucne.registrodeestudiantes.Data.Tareas.local.EstudianteDao
 import edu.ucne.registrodeestudiantes.Data.Tareas.local.EntityEstudiante
+import edu.ucne.registrodeestudiantes.Data.Tareas.local.AsignaturaDao
+import edu.ucne.registrodeestudiantes.Data.Tareas.local.AsignaturaEntity
 
-
-
-@Database(entities =[EntityEstudiante::class],
-    version = 1,
+@Database(entities = [EntityEstudiante::class, AsignaturaEntity::class],
+    version = 2,
     exportSchema = false)
-
 abstract class DBEstudiante: RoomDatabase(){
-
-    abstract fun EstudianteDao(): EstudianteDao
+    abstract fun estudianteDao(): EstudianteDao
+    abstract fun asignaturaDao(): AsignaturaDao
 }
