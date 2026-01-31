3aedba0d45f2ec5b3da2f19bd2edb6d76f93cb7f
Imanol-Acosta <ilacosta05@hotmail.com>
2026-01-31 12:19:45 -0400

    Modificando app modelo

--- app/src/main/java/edu/ucne/registrodeestudiantes/Data/di/AppModelo.kt
+++ app/src/main/java/edu/ucne/registrodeestudiantes/Data/di/AppModelo.kt
@@ -8,44 +8,50 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import edu.ucne.registrodeestudiantes.Data.Repository.EstudianteRepositoryImpl
+import edu.ucne.registrodeestudiantes.Data.Repository.AsignaturaRepositoryImpl
 import edu.ucne.registrodeestudiantes.Data.Tareas.local.EstudianteDao
-import edu.ucne.registrodeestudiantes.Repository.EstudianteRepository
+import edu.ucne.registrodeestudiantes.Data.Tareas.local.AsignaturaDao
+import edu.ucne.registrodeestudiantes.Domain.Estudiante.Repository.EstudianteRepository
+import edu.ucne.registrodeestudiantes.Domain.Asignatura.Repository.AsignaturaRepository
 import edu.ucne.registrodeestudiantes.Data.DataBase.DBEstudiante
-import jakarta.inject.Singleton
+import javax.inject.Singleton
 
 
-@InstallIn(
-    SingletonComponent::class)
+@InstallIn(SingletonComponent::class)
 @Module
-
 object AppModule {
     @Provides
     @Singleton
     fun provideEstudianteDB(@ApplicationContext appContext: Context) : DBEstudiante{
-
         return Room.databaseBuilder(
             appContext,
             DBEstudiante::class.java,
             "EstudianteDB"
-        ).fallbackToDestructiveMigration(false)
+        ).fallbackToDestructiveMigration()
             .build()
     }
 
     @Provides
     @Singleton
     fun provideEstudianteDao(estudianteDB: DBEstudiante) : EstudianteDao{
-        return estudianteDB.EstudianteDao()
+        return estudianteDB.estudianteDao()
     }
 
     @Provides
     @Singleton
-    fun provideEstudianteRepositoryImpl(estudianteDao: EstudianteDao) : EstudianteRepositoryImpl{
-        return EstudianteRepositoryImpl(estudianteDao)
+    fun provideAsignaturaDao(estudianteDB: DBEstudiante) : AsignaturaDao{
+        return estudianteDB.asignaturaDao()
     }
 
     @Provides
     @Singleton
     fun provideEstudianteRepository(impl: EstudianteRepositoryImpl): EstudianteRepository {
+        return impl
+    }
+
+    @Provides
+    @Singleton
+    fun provideAsignaturaRepository(impl: AsignaturaRepositoryImpl): AsignaturaRepository {
         return impl
     }
 }
