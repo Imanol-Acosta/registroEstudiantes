f7c3a6c85b604a2585b94b9018c56f02744b1f6a
Imanol-Acosta <ilacosta05@hotmail.com>
2026-02-01 15:19:51 -0400

    Eliminando archivos innecesarios

--- app/src/main/java/edu/ucne/registrodeestudiantes/Domain/Penalidad/Model/TipoPenalidad.kt
+++ app/src/main/java/edu/ucne/registrodeestudiantes/Domain/Penalidad/Model/TipoPenalidad.kt
@@ -1,7 +1,7 @@
 package edu.ucne.registrodeestudiantes.Domain.Penalidad.Model
 
 data class TipoPenalidad(
-    val tipoId: Int? = null,
+    val tipoId: Int = 0,
     val nombre: String,
     val descripcion: String,
     val puntosDescuento: Int
--- app/src/main/java/edu/ucne/registrodeestudiantes/Presentation/Asignatura/List/AsignaturaListUiState.kt
+++
@@ -1,8 +0,0 @@
-package edu.ucne.registrodeestudiantes.Presentation.Asignatura.List
-
-import edu.ucne.registrodeestudiantes.Domain.Asignatura.Model.Asignatura
-
-data class AsignaturaListUiState(
-    val asignaturas: List<Asignatura> = emptyList(),
-    val isLoading: Boolean = false
-)
